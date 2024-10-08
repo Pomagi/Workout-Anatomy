88d8beff15c9422c2f049060bbede94fb7ae7859
Pomagi <gvesel01@gmail.com>
2024-10-08 13:40:40 +0200

    Add ExerciseDAO class and implement method that returns names of all exercises

---
+++ src/main/java/database/ExerciseDAO.java
@@ -0,0 +1,32 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExerciseDAO {
+
+    public List<String> getAllExercises(){
+
+        List<String> exercises = new ArrayList<String>();
+
+        try(Connection connection = DatabaseConnection.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM exercises"))
+        {
+
+            while(resultSet.next()){
+                exercises.add(resultSet.getString("name"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return exercises;
+
+    }
+
+}
