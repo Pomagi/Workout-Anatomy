20fb906ffedcecb4e426c676cfcdb70e5c29e46d
Pomagi <gvesel01@gmail.com>
2024-10-08 13:26:32 +0200

    Implement database connection class Connection to local mysql

---
+++ src/main/java/database/DatabaseConnection.java
@@ -0,0 +1,21 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String url = "jdbc:mysql://localhost:3306/workout_anatomy";
+    private static final String user = "root";
+    private static final String password = "password";
+
+    public static Connection getConnection() {
+        try{
+            return DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
