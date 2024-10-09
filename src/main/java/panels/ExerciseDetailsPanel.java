7d1ba2a8c3996d1c2653ebc444927fab8b27eaee
Pomagi <gvesel01@gmail.com>
2024-10-09 17:18:43 +0200

    Implement showing image of muscle group on muscle group click

--- src/main/java/panels/ExerciseDetailsPanel.java
+++ src/main/java/panels/ExerciseDetailsPanel.java
@@ -4,17 +4,23 @@ import database.ExerciseDAO;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ExerciseDetailsPanel extends JPanel {
 
     ExerciseDAO exerciseDAO = new ExerciseDAO();
+
     String exerciseName;
     JLabel exerciseTitle;
+
     List<String> muscles;
     DefaultListModel<String> musclesModel;
     JList<String> musclesList;
+
+    JLabel muscleImageLabel;
 
 
 
@@ -40,10 +46,27 @@ public class ExerciseDetailsPanel extends JPanel {
         musclesList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         musclesList.setLayoutOrientation(JList.VERTICAL);
         musclesList.setFont(new Font("Arial", Font.PLAIN, 20));
+        musclesList.addMouseListener(new MouseAdapter() {
+           @Override
+           public void mouseClicked(MouseEvent e) {
+               if(e.getClickCount() == 1) {
+                   String selectedMuscle = musclesList.getSelectedValue();
+                   if(selectedMuscle != null) {
+                       updateMuscleImage(selectedMuscle);
+                   }
+               }
+           }
+        });
 
         JScrollPane musclesListScrollPane = new JScrollPane(musclesList);
         musclesListScrollPane.setPreferredSize(new Dimension(300, 500));
         panel.add(musclesListScrollPane, BorderLayout.WEST);
+
+        muscleImageLabel = new JLabel();
+        muscleImageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        muscleImageLabel.setVerticalAlignment(SwingConstants.CENTER);
+        panel.add(muscleImageLabel, BorderLayout.CENTER);
+
 
         add(panel, BorderLayout.CENTER);
 
@@ -61,6 +84,24 @@ public class ExerciseDetailsPanel extends JPanel {
 
         for(String muscle : muscles){
             musclesModel.addElement(muscle);
+        }
+    }
+
+    private void updateMuscleImage(String muscleName){
+
+        String imageURL = exerciseDAO.getMuscleGroupImageURL(muscleName);
+        System.out.println(imageURL);
+
+        if(imageURL != null){
+
+            ImageIcon muscleImage = new ImageIcon(getClass().getResource("/muscles/" + imageURL));
+            System.out.println(muscleImage);
+            Image scaledImage = muscleImage.getImage().getScaledInstance(500, 500, Image.SCALE_SMOOTH);
+            muscleImageLabel.setIcon(new ImageIcon(scaledImage));
+
+        }
+        else{
+            muscleImageLabel.setIcon(null);
         }
     }
 
