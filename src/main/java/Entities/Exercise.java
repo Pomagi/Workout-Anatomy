6340dc543d9f0565700d612a7d9e4d2bcae0472e
Pomagi <gvesel01@gmail.com>
2024-10-11 14:55:46 +0200

    Add entitie classes

---
+++ src/main/java/Entities/Exercise.java
@@ -0,0 +1,21 @@
+package Entities;
+
+public class Exercise {
+
+    private int id;
+    private String name;
+    private String video;
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getVideo(){
+        return video;
+    }
+
+}
---
+++ src/main/java/Entities/Muscle.java
@@ -0,0 +1,22 @@
+package Entities;
+
+public class Muscle {
+
+    private int id;
+    private String name;
+    private String image;
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getImage(){
+        return image;
+    }
+
+
+}
